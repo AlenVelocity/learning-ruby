701ba3911d46d6c2ea3e0994e3f41edd29c87154
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-07-16 18:58:15 +0530

    case/when > switch/case

---
+++ Day_1/13_Case.rb
@@ -0,0 +1,44 @@
+def cases(number)
+  case number
+    when 1
+        "January"
+    when 2
+        "February"
+    when 3
+        "March"
+    when 4
+        "April"
+    when 5
+        "May"
+    when 6
+        "June"
+    when 7
+        "July"
+    when 8
+        "August"
+    when 9
+        "September"
+    when 10
+        "October"
+    when 11
+        "November"
+    when 12
+        "December"
+    else
+        "Invalid"
+    end
+end
+
+puts cases(1)
+puts cases(2)
+puts cases(3)
+puts cases(4)
+puts cases(5)
+puts cases(6)
+puts cases(7)
+puts cases(8)
+puts cases(9)
+puts cases(10)
+puts cases(11)
+puts cases(12)
+puts cases(13)
