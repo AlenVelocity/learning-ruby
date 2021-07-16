fa32d5ab6bd4c91ed9416cd6113b12c05b1e69c8
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-07-16 18:22:15 +0530

    Strings are really cool

---
+++ Day_1/6_Strings.rb
@@ -0,0 +1,19 @@
+str = "Zelda"
+
+
+puts str.upcase
+puts str.downcase
+puts str.capitalize
+puts str.reverse
+puts str.reverse.upcase
+puts str.reverse.downcase
+puts str.reverse.capitalize
+puts str.reverse.reverse
+puts str.reverse.reverse.upcase
+puts str.reverse.reverse.downcase
+puts str.reverse.reverse.capitalize
+puts str.reverse.reverse.reverse
+puts str.reverse.reverse.reverse.upcase
+puts str.reverse.reverse.reverse.downcase
+puts str.reverse.reverse.reverse.capitalize
+puts str.reverse.reverse.reverse.reverse
