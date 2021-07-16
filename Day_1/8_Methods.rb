2a8651f5aa8bcba9b9f59a04f98f0548832a33ff
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-07-16 18:29:46 +0530

    Methods are A W E S O M E

---
+++ Day_1/8_Methods.rb
@@ -0,0 +1,20 @@
+def add(n1, n2) 
+    return n1 + n2
+end
+
+def subtract(n1, n2)
+    return n1 - n2
+end
+
+def multiply(n1, n2)
+    return n1 * n2
+end
+
+def divide(n1, n2)
+    return n1 / n2
+end
+
+puts add(1, 2)
+puts subtract(3, 1)
+puts multiply(2, 2)
+puts divide(4, 2)
